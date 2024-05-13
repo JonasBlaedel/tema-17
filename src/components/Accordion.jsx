17fd92af33706afb11ae3ca0fa89c21ca5093b17
JonasBlaedel <119848473+JonasBlaedel@users.noreply.github.com>
2024-05-13 13:19:52 +0200

    Vertical og QNA

---
+++ src/components/Accordion.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+function Accordion({ items }) {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const toggleAccordion = (index) => {
+    setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
+  };
+
+  return (
+    <div className="accordion">
+      {items.map((item, index) => (
+        <div key={index} className={`accordion-item ${activeIndex === index ? "active" : ""}`}>
+          <div className="accordion-header p" onClick={() => toggleAccordion(index)}>
+            <span>{item.heading}</span> <span>{activeIndex === index ? "-" : "+"}</span>
+          </div>
+          {activeIndex === index && <div className="accordion-content p">{item.content}</div>}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Accordion;
