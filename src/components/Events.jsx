65d4cc420011453adfab815236d86bdbda481372
JonasBlaedel <119848473+JonasBlaedel@users.noreply.github.com>
2024-05-17 09:58:29 +0200

    styling og generelle rettelser

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -6,6 +6,10 @@ function Events(props) {
 
   return (
     <div className="tickets">
+      <div class="heading" id="billetter">
+        <p>{props.subheader}</p>
+        <h2>{props.header}</h2>
+      </div>
       <div className="buttons">
         <ButtonFilter client:load btnRegion="sjaelland" name="Sjælland" setArea={setArea} area={area} />
         <ButtonFilter client:load btnRegion="fyn" name="Fyn" setArea={setArea} area={area} />
